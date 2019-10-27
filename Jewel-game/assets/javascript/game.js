b2e793742c48fb4d4fb0aca4d93e26fadf1e5188
Luke Klymshyn <unseenmountain@gmail.com>
2019-10-26 18:15:15 -0700

    added pictures and a click event onto the first picture

---
+++ Jewel-game/assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+// var counter = 50;
+// $("#jewel1").on("click", function() {
+
+//     // Each time the user clicks the crystal the counter goes up by 1.
+//     counter += 1;
+
+//     // We then output the number of times the crystal is clicked.
+//     alert("You clicked this crystal " + counter + " times!");
+
+//   });
