9f1e52b9bc96f4387c9179c0db6992d69f4b4b06
Luke Klymshyn <unseenmountain@gmail.com>
2019-10-28 20:26:32 -0700

    added character discriptions and player variables

---
+++ assets/javascript/enemy.js
@@ -0,0 +1,11 @@
+var enemy;
+
+function enemy(enemyType, health, power, agility, speed){
+
+    this.enemyType = enemyType;
+    this.health = health;
+    this.power = power;
+    this.agility = agility;
+    this.speed = speed;
+
+ }
---
+++ assets/javascript/player.js
@@ -0,0 +1,14 @@
+let player;
+ 
+ 
+ 
+ function Player(classType, health, power, agility, speed, defence){
+
+    this.classType = classType;
+    this.health = health;
+    this.power = power;
+    this.agility = agility;
+    this.speed = speed;
+    this.defence = defence;
+
+ }
