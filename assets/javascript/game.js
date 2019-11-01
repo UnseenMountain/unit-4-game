b09f44328aba25529a2c4e24621dcd18a56601ec
Luke Klymshyn <unseenmountain@gmail.com>
2019-10-31 19:05:33 -0700

    added player and section variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+var hasChossenCharacter = false;
+
+var defender = false;
+
+var character = {};
+
+var enemy = {};
+
+var enemysDefeated = 0;
+
+
+
+var luke = { 
+    name: "Luke Skywalker",
+    health: 120,
+
+    attack: 9,
+    baseAttack: 9
+};
+
+var V = { 
+    name: "V",
+    health: 120,
+
+    attack: 12,
+    baseAttack: 12
+};
+
+var captianPrice = { 
+    name: "Captian Price",
+    health: 100,
+
+    attack: 10,
+    baseAttack: 10
+};
+
+var keanuReeves = { 
+    name: "Keanu Reeves",
+    health: 160,
+
+    attack: 15,
+    baseAttack: 15
+};
+
